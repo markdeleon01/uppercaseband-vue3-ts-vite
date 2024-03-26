9fd2ef2ee788b6c0fde5a972efedd3fe094c51b0
Mark de Leon <mark_deleon@hotmail.com>
2024-03-26 17:35:27 +0800

    Updated content

--- src/pinia.ts
+++ src/pinia.ts
@@ -1,3 +1,30 @@
 import { createPinia } from 'pinia'
+import { markRaw } from 'vue'
+import router from './router'
+import type { Router } from 'vue-router'
 
 export const pinia = createPinia()
+
+pinia.use( ({ store }) => {
+    store.router = markRaw(router)
+})
+
+declare module 'pinia' {
+    interface PiniaCustomProperties {
+        router: Router
+    }
+}
+
+pinia.use( ({ store }) => {
+    store.$onAction( ({ store, name, args, after, onError }) => {
+        console.log(`[${store.$id}] action ${name}, args: ${JSON.stringify(args)}`)
+
+        after(result => {
+            console.log(`[${store.$id}] action ${name}, result: ${JSON.stringify(result)} `)
+        })
+
+        onError( error => {
+            console.error(`[${store.$id}] action ${name}, error: ${error}`)
+        })
+    })
+})
